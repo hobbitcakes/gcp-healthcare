a23b6c63e4d5b8c829cfe2edde3f93b8158f7041
Jared Peterson <jaredpeterson@google.com>
2024-10-10 17:10:56 -0500

    Condensed Cloud Run create into the deploy script. TMYK

--- tools/fhir-to-gcs-bundler/bin/deploy.sh
+++ tools/fhir-to-gcs-bundler/bin/deploy.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 REGISTRY="gcf-artifacts"
 IMAGE="${src_region}-docker.pkg.dev/${src_intake}/${REGISTRY}/fhir-to-gcs-bundler"
-gcloud run deploy fhir-to-gcs-bundler \
---image=$IMAGE:latest \
---region=us-central1 \
---project=${src_intake} \
---memory=4Gi \
+gcloud --project=$src_intake beta run deploy fhir-to-gcs-bundler \
+--image=${IMAGE}:latest \
+--region=$src_region \
+--no-allow-unauthenticated \
+--timeout=300 \
 --cpu=2 \
+--memory=4Gi \
+--no-cpu-throttling \
+--max-instances=100 \
+--concurrency=8 \
 --set-env-vars "FHIR_STORE=${FHIR_STORE},BUCKET=${BUCKET}"
